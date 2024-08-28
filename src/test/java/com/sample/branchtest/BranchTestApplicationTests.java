4aca35896d8d73a6d03548a6b8fd3e40e148a38b
rbska <rbska1234@naver.com>
2024-08-28 23:49:35 +0900

    branch test

---
+++ src/test/java/com/sample/branchtest/BranchTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.branchtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BranchTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
